cdb4906a6f238fb8c2f976f5e1dbb55f6be2e100
JoaoSilvestre2002 <92168242+JoaoSilvestre2002@users.noreply.github.com>
2021-12-14 19:54:07 -0300

    Add files via upload

---
+++ exdia14.12.2021.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int controle, variavel2, vetor, x;
+    variavel2=0;
+    x=5000;
+    for (controle=0;controle<x;controle++){
+        vetor=malloc(xsizeof(int));
+        vetor[controle]=variavel2;
+        variavel2++;
+        printf("%d \n", vetor[controle]);
+    }
+    return 0;
+}
+
