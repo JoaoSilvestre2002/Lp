3583d3f4dd19e423018290d40742ff15e3253a7d
JoaoSilvestre2002 <92168242+JoaoSilvestre2002@users.noreply.github.com>
2022-01-17 14:17:56 -0300

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+
+     float v2,v1;
+   scanf("%f", &v1);
+      scanf("%f", &v2);
+   printf("MEDIA = %.5f\n", (3.5*v1+7.5*v2)/(11));
+
+    return 0;
+}
