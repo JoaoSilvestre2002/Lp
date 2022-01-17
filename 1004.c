f78ee8fd161058e14e19222aa4aae20812e4e06b
JoaoSilvestre2002 <92168242+JoaoSilvestre2002@users.noreply.github.com>
2022-01-17 14:22:50 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int numero1, numero2, produto12;
+        scanf("%d", &numero1);
+            scanf("%d", &numero2);
+            produto12 = (numero1*numero2);
+            printf("PROD = %d\n", produto12);
+ 
+    return 0;
+}
