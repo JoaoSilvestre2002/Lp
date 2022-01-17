a7cf52be8ba011a270695034be6404ef5a6f136a
JoaoSilvestre2002 <92168242+JoaoSilvestre2002@users.noreply.github.com>
2022-01-17 14:13:24 -0300

    Create 1006.c

---
+++ 1006.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+int main() {
+
+float variavel3,variavel1,variavel2;
+
+scanf("%f", &variavel1);
+scanf("%f", &variavel2);
+scanf("%f", &variavel3);
+
+printf("MEDIA = %.1f\n",(variavel1/0.5+ 3*variavel2+variavel3/0.2)*(0.1));
+
+
+
+
+return 0;
+
+}
