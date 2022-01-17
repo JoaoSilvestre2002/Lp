6b10544c7e1753daebb660e0321aa6138de87a15
JoaoSilvestre2002 <92168242+JoaoSilvestre2002@users.noreply.github.com>
2022-01-17 14:06:51 -0300

    Create 1059.c

---
+++ 1059.c
@@ -0,0 +1,11 @@
+ #include <math.h>
+ #include <stdio.h>
+int main() {
+    int controle;
+  int fixonumero = 100;
+        for( controle =2; controle <= fixonumero; controle=controle+2)
+        {
+        printf("%d\n", controle);
+      }
+    return 0;
+}
