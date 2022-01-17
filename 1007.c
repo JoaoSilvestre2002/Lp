f66e8d875527d06a09620a373496a53bdfe6cbd2
JoaoSilvestre2002 <92168242+JoaoSilvestre2002@users.noreply.github.com>
2022-01-17 14:17:33 -0300

    Create 1007.c

---
+++ 1007.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+
+int v4,v2,v3,v1;
+
+scanf("%d",&v1);
+scanf("%d",&v2);
+scanf("%d",&v3);
+scanf("%d",&v4);
+
+printf("DIFERENCA = %d\n",(v2*v1-v4*v3));
+return 0;
+
+}
