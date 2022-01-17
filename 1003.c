d469f4bed3e5585434aeefff5f7ee0f2deb5a8f4
JoaoSilvestre2002 <92168242+JoaoSilvestre2002@users.noreply.github.com>
2022-01-17 14:23:16 -0300

    Create 1003.c

---
+++ 1003.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int number1,number2,result;
+    
+    scanf("%d", &number1);
+     scanf("%d", &number2);
+    
+    result = (number2+number1);
+    
+    printf("SOMA = %d\n", result);
+ 
+    return 0;
+}
